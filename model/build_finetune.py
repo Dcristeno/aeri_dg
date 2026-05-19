2d767e6b32d2a7522fe6ff2a048908467ac8d9e5
Guangz <18755104058@163.com>
2026-05-19 17:21:29 +0800

    Make differential bridge auxiliary

--- model/build_finetune.py
+++ model/build_finetune.py
@@ -130,10 +130,11 @@ class IRRA(nn.Module):
                     observed_delta,
                     dim=-1,
                 )
-                bridge_loss = (
+                bridge_loss = pair_loss.mean() + self.args.bridge_delta_weight * (
                     differential_pair_loss.mean()
-                    + self.args.bridge_delta_weight * delta_consistency_loss.mean()
+                    + delta_consistency_loss.mean()
                 )
+                ret["bridge_delta_pair"] = differential_pair_loss.mean()
                 ret["bridge_delta_term"] = delta_consistency_loss.mean()
             else:
                 raise ValueError(f"Unsupported bridge_mode: {self.args.bridge_mode}")
